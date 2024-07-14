9e6190817a43a33fa0be2a7a9797fb8835b244b8
Anson Tsao <tsaoa@acm.org>
2024-07-14 11:30:50 -0700

    Updated dependencies

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Update Cloudflare DNS with internal IP address
+
+# Note: internal-ip v8 has regression, does not return correcpt ip
--- src/ddns.ts
+++ src/ddns.ts
@@ -6,30 +6,35 @@ let lastIP = ''
 
 export async function updateDNS() {
   const ip = await internalIpV4()
-
   if (!ip || lastIP === ip) return
   lastIP = ip
 
-  const token = env.CLOUDFLARE_API_TOKEN
-  const cf = new Cloudflare({ token })
-  const zones = (await cf.zones.browse()) as any
-  const zoneId = zones?.result.find((z: any) => z.name === env.CLOUDFLARE_ZONE)
+  const apiToken = env.CLOUDFLARE_API_TOKEN
+  const cf = new Cloudflare({ apiToken })
+  const zones = await cf.zones.list()
+  const zone_id = zones?.result.find((z: any) => z.name === env.CLOUDFLARE_ZONE)
     ?.id
-  const records = (await cf.dnsRecords.browse(zoneId)).result
+  if (!zone_id) {
+    console.error('Zone not found')
+    return
+  }
+  const records = (await cf.dns.records.list({ zone_id })).result
   const AName = `${env.CLOUDFLARE_A_NAME}.${env.CLOUDFLARE_ZONE}`
   const record: any = records?.find((r: any) => r.name === AName)
   if (record) {
-    cf.dnsRecords.edit(zoneId, record.id, {
+    await cf.dns.records.edit(record.id, {
       type: 'A',
       name: env.CLOUDFLARE_A_NAME,
+      path_zone_id: zone_id,
       content: ip,
       ttl: 1,
       proxied: false,
     })
   } else {
-    cf.dnsRecords.add(zoneId, {
+    await cf.dns.records.create({
       type: 'A',
       name: env.CLOUDFLARE_A_NAME,
+      path_zone_id: zone_id,
       content: ip,
       ttl: 1,
       proxied: false,
